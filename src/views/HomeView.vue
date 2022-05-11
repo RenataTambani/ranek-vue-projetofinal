67cc98bcaf85adfcf55491c8141734683970de2d
RenataTambani <renatatambani@gmail.com>
2022-05-11 15:23:58 -0300

    header-footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    
+    <section>
+      <h1>Compre ou Venda</h1>
+      <ProdutosBuscar/>
+      <ProdutosLista/>
+    </section>
   </div>
 </template>
 
 <script>
 
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue"
+import ProdutosLista from "@/components/ProdutosLista.vue"
 
 export default {
-  
+  name: "HomeView",
+  components: ProdutosBuscar, ProdutosLista
 }
 </script>
+
+<style scoped>
+
+h1{
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
